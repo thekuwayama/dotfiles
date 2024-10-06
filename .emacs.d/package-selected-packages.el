d15ae121f4214fab8ed7ea93479980c64a0a39cf
thekuwayama <thekuwayama@gmail.com>
2024-10-06 16:00:19 +0900

    add rbs-mode

--- .emacs.d/package-selected-packages.el
+++ .emacs.d/package-selected-packages.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(rg github-browse-file python-mode format-sql racer web-mode company cmake-mode json-mode yasnippet toml-mode company dap-mode flycheck-rust rust-auto-use flycheck-golangci-lint use-package lsp-metals protobuf-mode lsp-ui lsp-mode terraform-mode rubocop rust-mode ruby-end dockerfile-mode yaml-mode ruby-mode markdown-mode go-mode flycheck auto-complete)))
+   '(rbs-mode rg github-browse-file python-mode format-sql racer web-mode company cmake-mode json-mode yasnippet toml-mode company dap-mode flycheck-rust rust-auto-use flycheck-golangci-lint use-package lsp-metals protobuf-mode lsp-ui lsp-mode terraform-mode rubocop rust-mode ruby-end dockerfile-mode yaml-mode ruby-mode markdown-mode go-mode flycheck auto-complete)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
